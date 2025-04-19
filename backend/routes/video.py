2085e1f585f67c5c6581b041cf721e20ca90eabc
Jayvnn <Jayden.vinh.nguyen@gmail.com>
2025-04-18 17:48:15 -0700

    Upload Photo Addition

--- backend/routes/video.py
+++ backend/routes/video.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, UploadFile, File
 import shutil
+import json
 import os
 from uuid import uuid4
+import cv2
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
+from utils.pose import detect_pose_landmarks
+
 
 router = APIRouter()
 UPLOAD_DIR = "uploads"
@@ -15,5 +20,13 @@ async def upload_file(file: UploadFile = File(...)):
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+
+    frame = cv2.imread(file_path)
+    if frame is None:
+        raise HTTPException(status_code=400, detail="Uploaded file is not a valid image/video frame")
     
-    return {"filename": new_filename}
+    pose_data = detect_pose_landmarks(frame)
+
+    return JSONResponse(content=pose_data)
+    
